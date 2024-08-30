aa4f7e37298f7a6d0742de4c09e7c5df22506d06
swaranjali01 <swaragawande26@gmail.com>
2024-08-30 13:33:50 +0530

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,25 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav>
-      <div>
-        <p>BSafe</p>
-        <ul>
-          <li>
-            <Link to="/incident-reporting">Incident Reporting</Link>
-          </li>
-          <li>
-            <Link to="/emergency-assistance">Emergency Assistance</Link>
-          </li>
-          <li>
-            <Link to="/track-location">Track Location</Link>
-          </li>
-          <li>
-            <Link to="/community">Community</Link>
-          </li>
-          <li>
-            <Link to="/chatbot">Chatbot</Link>
-          </li>
-        </ul>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <p style={{ marginRight: '20px' }}>BSafe</p>
+        <div style={{ display: 'flex', gap: '20px' }}>
+          <Link to="/incident-reporting">
+            Incident Reporting
+          </Link>
+          <Link to="/emergency-assistance">
+            Emergency Assistance
+          </Link>
+          <Link to="/track-location">
+            Track Location
+          </Link>
+          <Link to="/community">
+            Community
+          </Link>
+          <Link to="/chatbot">
+            Chatbot
+          </Link>
+        </div>
       </div>
     </nav>
   );
