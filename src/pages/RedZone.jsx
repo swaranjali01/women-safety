e3bc843ee0f1722842c62c808f3b74a437d29f81
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2024-09-04 16:44:42 +0530

    new commit

--- src/pages/RedZone.jsx
+++ src/pages/RedZone.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 function RedZone() {
   return (
-    <div> Red Zone </div>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Red Zone Alert</h1>
+      <div className="bg-red-500 text-white p-4 rounded-md">
+        <p>Warning: You are in a red zone area!</p>
+      </div>
+    </div>
   )
 }
 
